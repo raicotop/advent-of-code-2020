60545457d6273473a7a060cfde031a98514b263b
Your Name <you@example.com>
2021-02-11 22:41:02 +0100

    day 18 part 2

--- aoc18/src/aoc18/core.clj
+++ aoc18/src/aoc18/core.clj
@@ -18,7 +18,7 @@
             snd (second expr)
             rst (drop 2 expr)]
         (if (symbol? fst)
-          (evaluate ((resolve fst) acc (evaluate 0 snd)) rst)
+          (evaluate ((eval fst) acc (evaluate 0 snd)) rst)
           (evaluate (evaluate 0 fst) (rest expr)))))))
 
 (defn sol-1
@@ -29,10 +29,40 @@
        (read-string)
        (evaluate 0)))
 
+(defn evaluate-2
+  [acc expr]
+  (if (number? expr)
+    expr
+    (if (empty? expr)
+      acc
+      (let [fst (first expr)
+            snd (second expr)
+            trd (nth expr 2 nil)
+            fth (nth expr 3 nil)]
+        (if (symbol? fst)
+          (if (and (symbol? trd)
+                   (= * (eval fst))
+                   (= + (eval trd)))
+            (evaluate-2 acc (concat (vector fst (+ (evaluate-2 0 snd) (evaluate-2 0 fth))) (drop 4 expr)))
+            (evaluate-2 ((eval fst) acc (evaluate-2 0 snd)) (drop 2 expr)))
+          (evaluate-2 (evaluate-2 0 fst) (rest expr)))))))
+
+(defn sol-2
+  [s]
+  (->> (clojure.string/replace (clojure.string/replace s "(" "[") ")" "]")
+       (apply str)
+       (#(str "[" % "]"))
+       (read-string)
+       (evaluate-2 0)))
+
 (comment
   
   (map sol-1 demo)
   (->> (map sol-1 (clojure.string/split-lines (slurp "resources/input.txt")))
        (reduce +)) ;=> 45283905029161
+  
+  (map sol-2 demo)
+  (->> (map sol-2 (clojure.string/split-lines (slurp "resources/input.txt")))
+       (reduce +)) ;=> 216975281211165
 
 ,)
